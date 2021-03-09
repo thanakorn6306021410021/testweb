bc31bb42ec80e818515d6e3303f9becb274f06f0
thanakorn6306021410021 <6306021410021@fitm.kmutnb.ac.th>
2021-03-09 15:37:30 +0700

    cheate bookstore.sql

---
+++ bookstore.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2021 at 09:37 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `BookID` varchar(5) NOT NULL,
+  `BookName` varchar(50) NOT NULL,
+  `TypeID` char(3) NOT NULL,
+  `StatusID` char(2) NOT NULL,
+  `Publish` varchar(20) DEFAULT NULL,
+  `UnitPrice` int(4) NOT NULL,
+  `UnitRent` int(2) NOT NULL,
+  `DayAmount` int(2) DEFAULT NULL,
+  `Picture` varchar(30) DEFAULT NULL,
+  `BookDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`BookID`, `BookName`, `TypeID`, `StatusID`, `Publish`, `UnitPrice`, `UnitRent`, `DayAmount`, `Picture`, `BookDate`) VALUES
+('00001', 'Doraemon', '001', '01', 'Kpm', 150, 3, 2, '-', '2554-02-01'),
+('00002', 'เก็บตะวนั', '002', '03', 'WRP', 250, 5, 3, '-', '2554-03-25'),
+('00003', 'สิ่งมีชีวติ', '002', '01', 'YPR', 185, 3, 3, '-', '2553-03-25'),
+('00004', 'คู่สร้างคู่สม', '003', '01', 'DDR', 20, 1, 2, '-', '2552-01-15'),
+('00005', 'Konan', '001', '02', 'Kpm', 80, 2, 2, '-', '2554-04-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `CustomerID` varchar(4) NOT NULL,
+  `CustomerName` varchar(30) NOT NULL,
+  `CustomerSurname` varchar(30) NOT NULL,
+  `CustomerAddr` varchar(60) DEFAULT NULL,
+  `CustomerPhone` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`CustomerID`, `CustomerName`, `CustomerSurname`, `CustomerAddr`, `CustomerPhone`) VALUES
+('0001', 'สมชัย', 'เชียงพงศ์พันธุ์', 'ปราจีนบุรี', '037213219'),
+('0002', 'Jone', 'Smith', 'กรุงเทพ', '027341293');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statusbook`
+--
+
+CREATE TABLE `statusbook` (
+  `StatusID` char(2) NOT NULL,
+  `StatusName` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `statusbook`
+--
+
+INSERT INTO `statusbook` (`StatusID`, `StatusName`) VALUES
+('01', 'ปกติ'),
+('02', 'ช ารุด'),
+('03', 'ส่งซ่อม');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typebook`
+--
+
+CREATE TABLE `typebook` (
+  `TypeID` char(3) NOT NULL,
+  `TypeName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `typebook`
+--
+
+INSERT INTO `typebook` (`TypeID`, `TypeName`) VALUES
+('001', 'การ์ตูน'),
+('002', 'นิยาย'),
+('003', 'นิตยสาร');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`BookID`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `statusbook`
+--
+ALTER TABLE `statusbook`
+  ADD PRIMARY KEY (`StatusID`);
+
+--
+-- Indexes for table `typebook`
+--
+ALTER TABLE `typebook`
+  ADD PRIMARY KEY (`TypeID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

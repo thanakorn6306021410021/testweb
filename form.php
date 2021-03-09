89c45485b2f21948ec6aef2fcca8daca07bcf4f5
thanakorn6306021410021 <6306021410021@fitm.kmutnb.ac.th>
2021-03-09 14:34:02 +0700

    crate form.php

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test Gihub</title>
+</head>
+<body>
+ <form>
+ <p>NAME : 
+ <input type="text" name="name"> 
+ </p>
+ <p>age : 
+ <input type="text" name="age"> 
+ </p>
+ <input type="submit" name="submit">
+ </form>
+</body>
+</html>

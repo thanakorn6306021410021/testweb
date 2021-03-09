a9fefff8899afdf923aac7c49a885c9b58a4224a
thanakorn6306021410021 <6306021410021@fitm.kmutnb.ac.th>
2021-03-09 14:30:35 +0700

    create index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
 <html>
-  <body>
-      <h1>Hello</h1>
-  </body>
+<head>
+	<title>test Gihub</title>
+</head>
+<body>
+ <form>
+ <p>NAME : 
+ <input type="text" name="name"> 
+ </p>
+ <p>age : 
+ <input type="text" name="age"> 
+ </p>
+ <input type="submit" name="submit">
+ </form>
+</body>
 </html>
